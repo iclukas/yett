bac896b5cc36ad9c31088a9779cec82dca57b060
iclukas <1122393+iclukas@users.noreply.github.com>
2026-09-08 15:33:57 +0200

    Fix edge cases: data-type already set, type attribute empty

--- src/monkey.js
+++ src/monkey.js
@@ -23,7 +23,7 @@ document.createElement = function(...args) {
                 ...originalDescriptors.src,
                 set(value) {
                     if(isOnBlacklist(value, scriptElt.type)) {
-                        scriptElt.dataset.type = scriptElt.type;
+                        if (!scriptElt.dataset.type && scriptElt.type) scriptElt.dataset.type = scriptElt.type;
                         originalDescriptors.type.set.call(this, TYPE_ATTRIBUTE)
                     }
                     originalDescriptors.src.set.call(this, value)
